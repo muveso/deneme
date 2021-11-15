80924ae5e0d29bbda8d2739f7acfa7fba9b1619d
Ran Amos <amosran96@gmail.com>
2021-11-15 18:47:10 +0200

    Client class to seperated file

---
+++ Assets/Scripts/Communicators/Client.cs
@@ -0,0 +1,17 @@
+﻿using Evade.Utils.Network;
+
+namespace Evade.Communicators {
+    public class Client {
+        public Client(TcpClient tcpClient) {
+            TcpClient = tcpClient;
+            Details = new ClientDetails();
+        }
+
+        public ClientDetails Details { get; }
+        public TcpClient TcpClient { get; }
+
+        public override string ToString() {
+            return $"IP: {TcpClient} | {Details}";
+        }
+    }
+}
--- Assets/Scripts/Communicators/TcpServerCommunicator.cs
+++ Assets/Scripts/Communicators/TcpServerCommunicator.cs
@@ -35,18 +35,4 @@ namespace Evade.Communicators {
             }
         }
     }
-
-    public class Client {
-        public Client(TcpClient tcpClient) {
-            TcpClient = tcpClient;
-            Details = new ClientDetails();
-        }
-
-        public ClientDetails Details { get; }
-        public TcpClient TcpClient { get; }
-
-        public override string ToString() {
-            return $"IP: {TcpClient} | {Details}";
-        }
-    }
 }
