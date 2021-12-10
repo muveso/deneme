d69eef809ab35e52c17adf110641db92994d9882
panchock <amosran@gmail.com>
2021-12-10 13:15:55 +0200

    document

--- Assets/Scripts/Game/NetworkBehaviour.cs
+++ Assets/Scripts/Game/NetworkBehaviour.cs
@@ -7,6 +7,10 @@ namespace Assets.Scripts.Game {
     public abstract class NetworkBehaviour : MonoBehaviour {
         protected bool IsLocal { get; set; } = true;
 
+        /// <summary>
+        ///     FixedUpdate used in order to sync the input speed from clients.
+        ///     If one client will run on higher fps we don't want it to send more input to the server.
+        /// </summary>
         private void FixedUpdate() {
             if (IsLocal) {
                 var updateMessage = ClientUpdate();
