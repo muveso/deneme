0b1ffbf973629f48dfb14954c0f3a6e6ce428a46
panchock <amosran@gmail.com>
2022-01-04 16:51:17 +0200

    fix spawn

--- Assets/Scripts/Game/Initializer.cs
+++ Assets/Scripts/Game/Initializer.cs
@@ -3,6 +3,7 @@ using Assets.Scripts.General;
 using Assets.Scripts.Network.Common;
 using Tayx.Graphy;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Assets.Scripts.Game {
     public class Initializer : MonoBehaviour {
@@ -24,7 +25,7 @@ namespace Assets.Scripts.Game {
 
         private void CreateObstaclesObjects() {
             var serverClient = new ServerClient();
-            var startingPoint = GameObject.Find("StartingPoint").transform.position;
+            var startingPoint = GetStartingPoint().transform.position;
 
             for (var i = 1; i <= GameConsts.ObstacleCount; ++i) {
                 var obstacleOneObject = ObstacleOne.CreateObstacleOne(startingPoint + Vector3.forward * i * 30,
@@ -35,16 +36,35 @@ namespace Assets.Scripts.Game {
         }
 
         private void CreatePlayersObjects() {
-            var startingPoint = GameObject.Find("StartingPoint").transform.position;
             foreach (var client in GameManager.Instance.NetworkManagers.TcpServerManager.Clients) {
                 var playerObject =
-                    Player.CreatePlayer(startingPoint,
+                    Player.CreatePlayer(GetRandomPositionInStartingPoint(),
                         Guid.NewGuid().ToString(),
                         client.Details.Nickname,
                         HostClient.IsHostClient(client));
                 GameManager.Instance.ServerGameObjects[playerObject.name] =
                     new Tuple<GameObject, Client>(playerObject, client);
             }
+        }
+
+        private Vector3 GetRandomPositionInStartingPoint() {
+            var startingPoint = GetStartingPoint();
+            var verticesList = startingPoint.GetComponent<MeshFilter>().sharedMesh.vertices;
+
+            var rightUp = startingPoint.transform.TransformPoint(verticesList[0]);
+            var leftUp = startingPoint.transform.TransformPoint(verticesList[10]);
+            var rightBottom = startingPoint.transform.TransformPoint(verticesList[110]);
+
+            var random = new Random();
+            return new Vector3(
+                random.Next((int) leftUp.x, (int) rightUp.x),
+                leftUp.y,
+                random.Next((int) rightBottom.z, (int) rightUp.z)
+            );
+        }
+
+        private GameObject GetStartingPoint() {
+            return GameObject.Find("StartingPoint");
         }
     }
 }
