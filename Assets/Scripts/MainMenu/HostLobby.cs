d4498ae2cbdc848170de6f7c403063e049c4d7fe
panchock <amosran@gmail.com>
2022-02-12 17:52:34 +0200

    delete unnecessary functions

--- Assets/Scripts/MainMenu/HostLobby.cs
+++ Assets/Scripts/MainMenu/HostLobby.cs
@@ -29,17 +29,6 @@ namespace Assets.Scripts.MainMenu {
                 transform);
         }
 
-        private bool AreAllPlayersReady() {
-            foreach (var client in GameManager.Instance.NetworkManagers.TcpServerManager
-                         .Clients) {
-                if (!client.Details.IsReady) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
         private void InitializeCommunicatorAndServer() {
             ServerLobby serverLobby = gameObject.AddComponent<ServerLobby>();
             serverLobby.StartServer(IPInputField.text, PortInputField.text);
@@ -47,22 +36,6 @@ namespace Assets.Scripts.MainMenu {
             GameManager.Instance.NetworkManagers.ReliableClientManager =
                 new HostClientManager(GameManager.Instance.NetworkManagers.TcpServerManager,
                     GameManager.Instance.Nickname);
-        }
-
-
-        public void OnClickStartGame() {
-            TryToStartGame();
-        }
-
-        private void TryToStartGame() {
-            if (AreAllPlayersReady()) {
-                Debug.Log("Starting Game");
-                GameManager.Instance.NetworkManagers.TcpServerManager.Communicator.Send(new StartGameMessage());
-                SceneManager.LoadScene("Game");
-                Destroy(this);
-            } else {
-                Debug.Log("Not all clients ready");
-            }
         }
 
         public void OnClickConnect() {
